755dd3fba09a0144c57f160f3a1f4533c88e8453
aaaaapchy <lidija.verner@yandex.ru>
2025-12-19 19:30:13 +0700

    Изменение цвета фона

--- app/src/main/java/com/example/photogallery_lab10/MainActivity.kt
+++ app/src/main/java/com/example/photogallery_lab10/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.photogallery_lab10
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.photogallery_lab10.ui.theme.PhotoGallery_Lab10Theme
 
@@ -17,31 +23,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             PhotoGallery_Lab10Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                MaterialTheme(
+                    colorScheme = lightColorScheme(
+                        primary = Color(0xFF808080),
+                        surface = Color.Gray,
+                        background = Color.Gray
                     )
+                ) {
+                    Scaffold(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Gray)
+                    ) { innerPadding ->
+                        PhotoGalleryScreen(
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PhotoGalleryScreen(modifier: Modifier) {
+Text("Проверка")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhotoGallery_Lab10Theme {
-        Greeting("Android")
-    }
-}
